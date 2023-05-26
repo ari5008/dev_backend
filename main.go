ae2d9b3beae571bd2e0ead01a9e5a677611ef0fd
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-05-26 15:51:02 +0900

    feat: mainにtrackを追加する #8

--- main.go
+++ main.go
@@ -15,20 +15,23 @@ func main() {
 	dbConn := db.NewDB()
 	defer fmt.Println("successful")
 	defer db.CloseDB(dbConn)
-	dbConn.AutoMigrate(&model.User{}, &model.Account{})
+	dbConn.AutoMigrate(&model.User{}, &model.Account{}, &model.Track{})
 
 	useRepository := repository.NewUserRepository(dbConn)
 	accountRepository := repository.NewAccountRepository(dbConn)
+	trackRepository := repository.NewTrackRepository(dbConn)
 
 	userValidator := validator.NewUserValidator()
 	accountValidator := validator.NewAccountValidator()
 
 	userUsecase := usecase.NewUserUsecase(useRepository, userValidator)
 	accountUsecase := usecase.NewAccountUsecase(accountRepository, accountValidator)
+	trackUsecase := usecase.NewTrackUsecase(trackRepository)
 
 	userController := controller.NewUserController(userUsecase, accountUsecase)
 	accountController := controller.NewAccountController(accountUsecase)
+	trackController := controller.NewTrackController(trackUsecase, accountUsecase)
 
-	e := router.NewRouter(userController, accountController)
+	e := router.NewRouter(userController, accountController, trackController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
