d36dca89ec23e688f22d1463b980b010e4ee32a5
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-06-04 18:38:02 +0900

    feat: mainにlikeflag新規追加 #8

--- main.go
+++ main.go
@@ -15,11 +15,12 @@ func main() {
 	dbConn := db.NewDB()
 	defer fmt.Println("successful")
 	defer db.CloseDB(dbConn)
-	dbConn.AutoMigrate(&model.User{}, &model.Account{}, &model.Track{})
+	dbConn.AutoMigrate(&model.User{}, &model.Account{}, &model.Track{}, &model.Likeflag{})
 
 	useRepository := repository.NewUserRepository(dbConn)
 	accountRepository := repository.NewAccountRepository(dbConn)
 	trackRepository := repository.NewTrackRepository(dbConn)
+	likeFlagRepository := repository.NewLikeFlagRepository(dbConn)
 
 	userValidator := validator.NewUserValidator()
 	accountValidator := validator.NewAccountValidator()
@@ -27,11 +28,13 @@ func main() {
 	userUsecase := usecase.NewUserUsecase(useRepository, userValidator)
 	accountUsecase := usecase.NewAccountUsecase(accountRepository, accountValidator)
 	trackUsecase := usecase.NewTrackUsecase(trackRepository)
+	likeFlagUsecase := usecase.NewLikeFlagUsecase(likeFlagRepository)
 
 	userController := controller.NewUserController(userUsecase, accountUsecase)
 	accountController := controller.NewAccountController(accountUsecase)
 	trackController := controller.NewTrackController(trackUsecase, accountUsecase)
+	likeFlagController := controller.NewLikeFlagController(likeFlagUsecase, accountUsecase)
 
-	e := router.NewRouter(userController, accountController, trackController)
+	e := router.NewRouter(userController, accountController, trackController, likeFlagController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
