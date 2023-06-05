10198542ae5f00ba3b382339d58903f64f0865d5
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-06-05 16:39:11 +0900

    chore: account_idの表記変更 #14

--- model/track.go
+++ model/track.go
@@ -8,9 +8,8 @@ type Track struct {
 	ArtistName  string    `json:"artist_name"`
 	JacketImage string    `json:"jacket_image"`
 	Genre       string    `json:"genre"`
-	Comment     string		`json:"comment`
-	Likes       uint      `json:"likes"`
-	Account     Account   `json:"account" gorm:"foreignKey:AccountId; constraint:OnDelete:CASCADE"`
+	Comment     string    `json:"comment"`
+	Likes       int       `json:"likes"`
 	AccountId   uint      `json:"account_id" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
@@ -22,8 +21,9 @@ type TrackResponse struct {
 	ArtistName  string    `json:"artist_name"`
 	JacketImage string    `json:"jacket_image"`
 	Genre       string    `json:"genre"`
-	Comment     string		`json:"comment`
-	Likes       uint      `json:"likes"`
+	Comment     string    `json:"comment"`
+	Likes       int       `json:"likes"`
+	AccountId   uint      `json:"account_id" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
