d50f85d09b15bbd9109c34ffada701f45f393d2d
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-06-16 10:36:35 +0900

    feat: track_validatorを新規作成 #25

---
+++ validator/track_validator.go
@@ -0,0 +1,49 @@
+package validator
+
+import (
+	"backend/model"
+	"backend/repository"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type ITrackValidator interface {
+	TrackValidate(track model.Track) error
+}
+
+type trackValidator struct{
+	tr repository.ITrackRepository
+}
+
+func NewTrackValidator(tr repository.ITrackRepository) ITrackValidator {
+	return &trackValidator{tr}
+}
+
+func (tv *trackValidator) TrackValidate(track model.Track) error {
+	err := tv.tr.NotSameTitleAndAccountID(&track)
+	if err != nil {
+		return err
+	}
+	err = tv.tr.NotSameTrack(&track)
+	if err != nil {
+		return err
+	}
+	return validation.ValidateStruct(&track,
+		validation.Field(
+			&track.Title,
+			validation.Required.Error("require title"),
+		),
+		validation.Field(
+			&track.ArtistName,
+			validation.Required.Error("require artist_name"),
+		),
+		validation.Field(
+			&track.JacketImage,
+			validation.Required.Error("require jacket_image"),
+		),
+		validation.Field(
+			&track.Genre,
+			validation.Required.Error("require genre"),
+		),
+	)
+}
