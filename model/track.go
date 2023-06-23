c5dbd32d33d140b1b909cca10f42e546f3009af1
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-06-23 17:56:48 +0900

    feat: trackModelにexternal_urlを追加 #14

--- model/track.go
+++ model/track.go
@@ -3,27 +3,29 @@ package model
 import "time"
 
 type Track struct {
-	ID          uint      `json:"id" gorm:"primaryKey"`
-	Title       string    `json:"title" gorm:"not null"`
-	ArtistName  string    `json:"artist_name"`
-	JacketImage string    `json:"jacket_image"`
-	Genre       string    `json:"genre"`
-	Comment     string    `json:"comment"`
-	Likes       int       `json:"likes"`
-	AccountId   uint      `json:"account_id" gorm:"not null"`
-	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	ID           uint      `json:"id" gorm:"primaryKey"`
+	Title        string    `json:"title" gorm:"not null"`
+	ArtistName   string    `json:"artist_name"`
+	JacketImage  string    `json:"jacket_image"`
+	Genre        string    `json:"genre"`
+	Comment      string    `json:"comment"`
+	Likes        int       `json:"likes"`
+	External_url string    `json:"external_url"`
+	AccountId    uint      `json:"account_id" gorm:"not null"`
+	CreatedAt    time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 type TrackResponse struct {
-	ID          uint      `json:"id" gorm:"primaryKey"`
-	Title       string    `json:"title" gorm:"not null"`
-	ArtistName  string    `json:"artist_name"`
-	JacketImage string    `json:"jacket_image"`
-	Genre       string    `json:"genre"`
-	Comment     string    `json:"comment"`
-	Likes       int       `json:"likes"`
-	AccountId   uint      `json:"account_id" gorm:"not null"`
-	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	ID           uint      `json:"id" gorm:"primaryKey"`
+	Title        string    `json:"title" gorm:"not null"`
+	ArtistName   string    `json:"artist_name"`
+	JacketImage  string    `json:"jacket_image"`
+	Genre        string    `json:"genre"`
+	Comment      string    `json:"comment"`
+	Likes        int       `json:"likes"`
+	External_url string    `json:"external_url"`
+	AccountId    uint      `json:"account_id" gorm:"not null"`
+	CreatedAt    time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
